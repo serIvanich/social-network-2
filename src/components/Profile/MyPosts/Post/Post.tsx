8f4259d2c5730ae90764c3e23f66e83f87819c9b
serIvanich <serivan1108@gmail.com>
2021-05-29 21:55:01 +0300

    componentDidMount

--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import s from './../MyPosts.module.css'
+import avatar from './../../../../assets/images/users3.png'
 
 type PostPropsType = {
     message: string
@@ -9,7 +10,7 @@ type PostPropsType = {
 export const Post: React.FC<PostPropsType> = React.memo(({message, likesCount}) => {
     return (
         <div className={s.post}>
-            <img src={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxukAEfWgGFG2o--9YtkAS_kJjHN5PiIE7pQ&usqp=CAU'} />
+            <img src={avatar} />
             {message}
             <div>
                 like: {likesCount}
--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -21,11 +21,11 @@ type ResponseType = {
 
 class Users extends React.Component<UsersPropsType, UserType[]> {
 
-    constructor(props: any) {
-        super(props);
+    componentDidMount() {
         axios.get('https://social-network.samuraijs.com/api/1.0/users').then((response: any) => {
-            props.setUsers(response.data.items)
+            this.props.setUsers(response.data.items)
         })
+
     }
 
     render() {
