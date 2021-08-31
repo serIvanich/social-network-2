7bbeaaa702e82e76787216adaae531d4311414d3
serIvanich <serivan1108@gmail.com>
2021-08-31 18:23:48 +0300

    trying to do ProfileStatus.test

---
+++ src/components/Profile/MyPosts/ProfileInfo/ProfileStatus.test.tsx
@@ -0,0 +1,15 @@
+// import React from "react";
+//
+// import ProfileStatus from "./ProfileStatus";
+//
+//
+//
+//
+// describe('ProfileStatus component', () => {
+//     test('status from props should be in the state', () => {
+//         const mockCallback = jest.fn()
+//         const component = create(<ProfileStatus status={'it-kamasutra'} changeUserProfileStatus={mockCallback}/>)
+//         const instance = component.getInstance()
+//         expect(instance.state.status).toBe('it-kamasutra')
+//     })
+// })
--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,15 @@ import React from 'react';
 import ReactDOM from "react-dom";
 import {BrowserRouter} from "react-router-dom";
 import App from "./App";
-import store from './redux/store';
+import  store  from './redux/store';
 import {Provider} from "react-redux";
 import SamuraiJsApp from "./App";
 
 
-ReactDOM.render(
-    <SamuraiJsApp/>
-    , document.getElementById('root'))
+
+    ReactDOM.render(
+<SamuraiJsApp/>
+        , document.getElementById('root'))
 
 
 
